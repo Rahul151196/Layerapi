56159df48178593255f8764b8684aff429ba9ba6
Rahul Raghuwanshi <87165754+Rahul151196@users.noreply.github.com>
2021-10-03 18:19:10 +0530

    Add files via upload

---
+++ BussinessLayer/Helper/ApplicationMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataLayer.Data;
+using DataLayer.Models;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Helper
+{
+    class ApplicationMapper : Profile
+    {
+        public ApplicationMapper()
+        {
+            CreateMap<Books, BookModel>().ReverseMap();
+        }
+    }
+}
---
+++ DataLayer/Repository/IBookRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataLayer.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repository
+{
+   public interface IBookRepository
+    {
+        Task<List<BookModel>> GetAllBooksAsync();
+        Task<BookModel> GetBooksByIdAsync(int bookId);
+        Task<int> AddBookAsync(BookModel bookmodel);
+        Task UpdateBookAsync(int bookId, BookModel bookModel);
+        Task UpdateBookPatchAsync(int bookId, JsonPatchDocument bookModel);
+        Task DeleteBookAsync(int bookId);
+    }
+}
